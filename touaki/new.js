e27055ce8133ad77043d3abb03639c10719c28b0
Rahaydn <lexea53@gmail.com>
2024-05-08 05:16:27 +0800

    5555

--- touaki/new.js
+++ touaki/new.js
@@ -1,23 +1,44 @@
-document.getElementById('btnHam').addEventListener('click', function () {
-    var menu = document.getElementById('menu');
-    menu.classList.toggle('show');
+//導覽列按鈕
+        document.getElementById('btnHam').addEventListener('click', function () {
+        var menu = document.getElementById('menu');
+        menu.classList.toggle('show');
+        });
+        
+//絲帶控制
+    const ribbon = document.querySelector('.ribbon');
+    const ribbons = document.querySelector('.ribbons');
+
+    function handleResize() {
+        if (window.innerWidth >= 768) {
+            ribbon.style.right = '0px';
+            ribbons.style.right = '0px';
+            ribbon.removeEventListener('mouseout', handleMouseOut);
+            ribbons.removeEventListener('mouseout', handleMouseOut);
+        } else {
+            ribbon.style.right = '-100px';
+            ribbons.style.right = '-100px';
+            ribbon.addEventListener('mouseout', handleMouseOut);
+            ribbons.addEventListener('mouseout', handleMouseOut);
+        }
+    }
+    function handleMouseOut() {
+        ribbon.style.right = '-100px';
+        ribbons.style.right = '-100px';
+    }
+    handleResize();
+
+    window.addEventListener('resize', () => {
+        handleResize();
     });
 
-const ribbon = document.getElementById('imgBoard');
-const ribbons = document.getElementById('imgBoards');
+    ribbon.addEventListener('mouseover', () => {
+        ribbon.style.right = '0px';
+    });
 
-ribbon.addEventListener('mouseover', () => {
-    ribbon.style.right = '0px';
-});
+    ribbon.addEventListener('mouseout', handleMouseOut);
 
-ribbon.addEventListener('mouseout', () => {
-    ribbon.style.right = '-100px';
-});
+    ribbons.addEventListener('mouseover', () => {
+        ribbons.style.right = '0px';
+    });
 
-ribbons.addEventListener('mouseover', () => {
-    ribbons.style.right = '0px';
-});
-
-ribbons.addEventListener('mouseout', () => {
-    ribbons.style.right = '-100px';
-});
+    ribbons.addEventListener('mouseout', handleMouseOut);
