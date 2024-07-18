06a15eb27a79d3c0cb8e7ca3be3f1acd9e9df21b
Rahaydn <lexea53@gmail.com>
2024-07-18 23:38:41 +0800

    新增網頁，css修改

--- touaki/new.js
+++ touaki/new.js
@@ -5,46 +5,58 @@
         });
         
 //絲帶控制
+// 獲取 DOM 元素
 const ribbon = document.querySelector('.ribbon');
 const ribbons = document.querySelector('.ribbons');
 
-function handleResize() {
-    if (window.innerWidth >= 768) {
-        ribbon.style.right = '0px';
-        ribbons.style.right = '0px';
-        ribbon.removeEventListener('mouseout', handleMouseOut);
-        ribbons.removeEventListener('mouseout', handleMouseOut);
-    } else {
-        ribbon.style.right = '-100px';
-        ribbons.style.right = '-100px';
-        ribbon.addEventListener('mouseout', handleMouseOut);
-        ribbons.addEventListener('mouseout', handleMouseOut);
-    }
-}
-
-function handleMouseOut() {
+// 定義初始化絲帶位置的函數
+function initializeRibbons() {
+    // 初始化位置為 -100px
     ribbon.style.right = '-100px';
     ribbons.style.right = '-100px';
 }
 
-// 首次加载时立即执行一次，根据窗口大小决定是否执行
+// 定義鼠標移出事件的處理函數
+function handleMouseOut() {
+    // 當鼠標移出時，將絲帶隱藏到右側邊緣外
+    ribbon.style.right = '-100px';
+    ribbons.style.right = '-100px';
+}
+
+// 首次加載時初始化絲帶位置
 window.addEventListener('load', () => {
-    handleResize();
+    initializeRibbons();
 });
 
-// 窗口大小改变时执行
-window.addEventListener('resize', () => {
-    handleResize();
-});
-
+// 當鼠標移到絲帶上時，將絲帶顯示到右側邊緣
 ribbon.addEventListener('mouseover', () => {
     ribbon.style.right = '0px';
 });
 
+// 添加鼠標移出事件的監聽器
 ribbon.addEventListener('mouseout', handleMouseOut);
 
+// 當鼠標移到另一個絲帶上時，將其顯示到右側邊緣
 ribbons.addEventListener('mouseover', () => {
     ribbons.style.right = '0px';
 });
 
+// 添加鼠標移出事件的監聽器
 ribbons.addEventListener('mouseout', handleMouseOut);
+
+
+//手風琴
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+    });
+}
